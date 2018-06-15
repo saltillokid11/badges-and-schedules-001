c3c1b4411ce166ccc5ca15daecf98cca7b4c2269
Tanner Lancaster <tanner@flatironschool.com>
2018-06-15 04:26:02 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map do |name|
+    badge_maker(name)
+  end
+end
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  end
+end
+
+def printer(atendees)
+  batch_badge_creator(atendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(atendees).each do |assign|
+    puts assign
+  end
+end
